8eac0678a93f98050ce326adbd7113408f8a91e0
Wren Liang <wren.liang@icloud.com>
2019-02-06 15:42:40 -0800

    initial commit

--- Stopwatch App/ViewController.swift
+++ Stopwatch App/ViewController.swift
@@ -10,11 +10,65 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var pauseButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    
+    
+    var counter = 0.0
+    var timer = Timer()
+    var isPlaying = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        timeLabel.text = String(counter)
+        pauseButton.isEnabled = false
     }
-
-
+    
+    
+    
+    @IBAction func startTimer(_ sender: AnyObject) {
+        if(isPlaying){
+            return
+        }
+        
+        startButton.isEnabled = false
+        pauseButton.isEnabled = true
+        
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target:self, selector: #selector(UpdateTimer), userInfo: nil, repeats: true)
+        
+        isPlaying = true
+    }
+    
+    @IBAction func pauseTimer(_ sender: AnyObject) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
+        
+    }
+    
+    
+    @IBAction func resetTimer(_ sender: AnyObject) {
+        startButton.isEnabled = true
+        pauseButton.isEnabled = false
+        
+        timer.invalidate()
+        isPlaying = false
+        counter = 0.0
+        timeLabel.text = String(counter)
+    }
+    
+    @objc func UpdateTimer(){
+        counter = counter + 0.1
+        timeLabel.text = String(format: "%.1f", counter)
+    }
+    
+    
+    
 }
 
